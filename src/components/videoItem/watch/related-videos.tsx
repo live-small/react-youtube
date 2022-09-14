c2cf80295c2663ee47361fc3243ee1435dad9a51
suhwa <hansuhwa2005@gmail.com>
2022-09-14 17:45:35 +0900

    RelatedVideos: 클릭한 영상과 관련된 영상 UI 구현

---
+++ src/components/videoItem/watch/related-videos.tsx
@@ -0,0 +1,58 @@
+import Video from "@components/videoItem/video";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { VideoType, Youtube } from "types/youtube";
+import LandingVideoExplain from "@components/videoItem/landing-video-explain";
+
+const Container = styled.article`
+	display: flex;
+	flex-direction: column;
+
+	.video-list {
+		display: flex;
+		flex-direction: column;
+	}
+`;
+
+// suspense 적용하기 **
+export default function RelatedVideos({
+	videoId,
+	youtube,
+}: {
+	videoId: string;
+	youtube: Youtube;
+}) {
+	const [relatedVideo, setRelatedVideo] = useState<VideoType[]>();
+
+	useEffect(() => {
+		youtube //
+			.getRelatedVideo(videoId)
+			.then((list) => setRelatedVideo(list));
+	}, [videoId, youtube]);
+
+	return (
+		<Container>
+			<section>관련 콘텐츠</section>
+			<section className="video-list">
+				{relatedVideo ? (
+					relatedVideo.map((video) => (
+						<Video
+							key={video.id}
+							video={video}
+							explainOfVideo={
+								<LandingVideoExplain video={video} />
+							}
+							layout={{
+								videoWidth: "100%",
+								margin: "10px 0 10px 0",
+								thumbnailAndExplainDirection: "row",
+							}}
+						/>
+					))
+				) : (
+					<div>loading...</div>
+				)}
+			</section>
+		</Container>
+	);
+}
--- src/pages/watch.tsx
+++ src/pages/watch.tsx
@@ -1,3 +1,19 @@
-export default function Watch() {
-	return <div>Watch</div>;
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { VideoType, Youtube } from "types/youtube";
+import RelatedVideos from "@components/videoItem/watch/related-videos";
+
+const Container = styled.main`
+	display: flex;
+	justify-content: center;
+`;
+
+export default function Watch({ youtube }: { youtube: Youtube }) {
+	const video = useLocation().state as VideoType;
+
+	return (
+		<Container>
+			<RelatedVideos videoId={video.id} youtube={youtube} />
+		</Container>
+	);
 }
