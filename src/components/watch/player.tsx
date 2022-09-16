294fff45603f4e0b64eddfea0d691e0cd57da513
suhwa <hansuhwa2005@gmail.com>
2022-09-16 17:26:52 +0900

    Player: 설명 더보기 구현
    video tag가 5개 이상일 경우, 설명 더보기에 전체 tag 보여줄 수 있도록함

--- src/components/watch/player.tsx
+++ src/components/watch/player.tsx
@@ -1,9 +1,11 @@
 import ChannelMetadata from "@components/watch/channel-metadata";
+import Tag from "@components/watch/tag";
 import VideoDescription from "@components/watch/video-description";
+import { useState } from "react";
 import styled from "styled-components";
 import { VideoType } from "types/youtube";
 
-const Container = styled.article`
+const Container = styled.article<{ isShowDescription: boolean }>`
 	display: flex;
 	flex-direction: column;
 	line-height: 15px;
@@ -19,6 +21,33 @@ const Container = styled.article`
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		padding-bottom: 16px;
+
+		.see-more {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			margin-left: 60px;
+			font-size: 14px;
+
+			.content {
+				max-width: 615px;
+				white-space: pre-wrap;
+				height: ${(props) => (props.isShowDescription ? null : "60px")};
+				overflow: hidden;
+				flex-direction: column;
+
+				.tag {
+					padding: 0.5rem 0 0.5rem 0;
+				}
+			}
+
+			.description-show-toggle-button {
+				font-weight: 500;
+				background-color: ${(props) => props.theme.color.gray};
+				color: darkgray;
+				padding: 0;
+			}
+		}
 	}
 `;
 
@@ -28,9 +57,13 @@ export default function Player({ video }: { video: VideoType }) {
 		statistics: { viewCount },
 		channel,
 	} = video;
+	const [isShowDescription, setIsShowDescription] = useState(false);
 
 	return (
-		<Container className="player-container">
+		<Container
+			className="player-container"
+			isShowDescription={isShowDescription}
+		>
 			<section className="player">
 				<iframe
 					id="ytplayer"
@@ -50,7 +83,20 @@ export default function Player({ video }: { video: VideoType }) {
 			/>
 			<section className="channel">
 				{channel && <ChannelMetadata channel={channel} />}
-				<div className="see-more">{description}</div>
+				<div className="see-more">
+					<div className="content">
+						{description}
+						<div className="tag">
+							{tags.length > 5 && <Tag tags={tags} />}
+						</div>
+					</div>
+					<button
+						className="description-show-toggle-button"
+						onClick={() => setIsShowDescription(!isShowDescription)}
+					>
+						{isShowDescription ? "간략히" : "더보기"}
+					</button>
+				</div>
 			</section>
 		</Container>
 	);
--- src/components/watch/tag.tsx
+++ src/components/watch/tag.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
 	display: flex;
+	flex-wrap: wrap;
 
 	a {
 		color: #065fd4;
